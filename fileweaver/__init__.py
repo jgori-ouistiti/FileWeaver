a441cfb2601a885b76f575be083f344b859df671
jgori <juliengori@gmail.com>
2022-05-11 12:58:11 +0200

    added makedirs() for .cookbook in init

--- fileweaver/__init__.py
+++ fileweaver/__init__.py
@@ -5,8 +5,13 @@ config = configparser.RawConfigParser()
 config.read("conf.cfg")
 
 PATH_TO_LIBS = config.get("Main", "PATH_TO_LIBS")
+PATH_TO_LINKS = config.get("FW-paths", "PATH_TO_LINKS")
 
 os.environ["CONFIG_FILE"] = os.path.abspath(PATH_TO_LIBS + "/conf.cfg")
+
+
+os.makedirs(PATH_TO_LINKS, exist_ok=True)
+
 
 from .base import cooking
 from .base import graph
@@ -15,4 +20,3 @@ from .base import managing
 
 from .read_write import readwrite
 from .read_write import map_incoming_messages
-
